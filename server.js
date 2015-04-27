81540d02a0f6215328d1c3ccc25fa1ddd6c76f9d
Jason Fan <jason.fan.74@gmail.com>
2015-04-27 12:33:25 -0400

    rem index

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express  = require('express');
 var app = express();
 app.use(express.static(__dirname));
-app.get("/", function(req,res){
-    res.send("hello");
-});
+//app.get("/", function(req,res){
+//    res.send("hello");
+//});
 console.log("Visualizing the world on port 8000");
 app.listen(process.env.port || 8000);
